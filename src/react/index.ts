380703a81d64769d24966f6d9097cc36c4771896
qfl1ck32 <andrei.rusu@cultofcoders.com>
2021-07-05 20:33:42 +0300

    Fix missing exports

--- src/react/index.ts
+++ src/react/index.ts
@@ -4,5 +4,6 @@ export * from "./smart";
 export * from "./XBrowserRouter";
 export * from "./XUIProvider";
 export * from "./smarts";
+export * from "./services";
 export * as Components from "./components";
 export { IComponents } from "./components/types";
